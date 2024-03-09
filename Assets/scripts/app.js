3c821494d216f2b99abfdc47557adc725cefd3ce
pointlessbish <87618683+pointlessbish@users.noreply.github.com>
2024-03-09 15:34:34 +0100

    cleaned up code waiting for info update

--- Assets/scripts/app.js
+++ Assets/scripts/app.js
@@ -1,24 +1,25 @@
 
-document.documentElement.setAttribute("data-theme", "dark");
+document.documentElement.setAttribute( "data-theme", "dark" );
 
 const themeMap = {
   dark: "light",
   light: "dark"
 };
 
-const theme = localStorage.getItem('theme')
-  || (tmp = Object.keys(themeMap)[0],
-    localStorage.setItem('theme', tmp),
-    tmp);
+const theme = localStorage.getItem( 'theme' )
+  || ( tmp = Object.keys( themeMap )[ 0 ],
+    localStorage.setItem( 'theme', tmp ),
+    tmp );
 const bodyClass = document.body.classList;
-bodyClass.add(theme);
+bodyClass.add( theme );
 
-function toggleTheme() {
-  const current = localStorage.getItem('theme');
-  const next = themeMap[current];
+function toggleTheme ()
+{
+  const current = localStorage.getItem( 'theme' );
+  const next = themeMap[ current ];
 
-  bodyClass.replace(current, next);
-  localStorage.setItem('theme', next);
+  bodyClass.replace( current, next );
+  localStorage.setItem( 'theme', next );
 }
 
-document.getElementById('themeButton').onclick = toggleTheme;
+document.getElementById( 'themeButton' ).onclick = toggleTheme;
